8128b954c1269760ab1b884f9842527830a6efb9
Matt Ellis <mellis@tidelift.com>
2018-02-13 19:16:41 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+ruby "2.5.0"
+
+git_source :github do |repo_name|
+  "git@github.com:#{repo_name}.git"
+end
+
+gem "activesupport"
+gem "jwt"
+gem "puma"
+gem "sinatra"
+gem "sinatra-contrib"
+gem "private_address_check"
+
+group :development, :test do
+  gem "minitest"
+  gem "pry"
+  gem "rack-test"
+  gem "rake"
+  gem "rspec"
+  gem "rubocop"
+  gem "simplecov"
+end
---
+++ app.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+require "sinatra/base"
+require "sinatra/multi_route"
+require "sinatra/reloader"
+require "sinatra/cookies"
+require "private_address_check"
+
+module AuthService
+  # Sinatra app to perform authentication related user actions
+  class App < Sinatra::Base
+    configure do
+      register Sinatra::MultiRoute
+      enable :sessions
+      enable :logging
+    end
+
+    configure :development do
+      require "pry"
+      set :port, 4568
+      set :host, "http://localhost:4568"
+      register Sinatra::Reloader
+      enable :dump_errors, :raise_errors
+    end
+
+    configure :production do
+      set :raise_errors, false
+      set :show_exceptions, false
+    end
+
+    get "/check/:address" do
+      { resp: PrivateAddressCheck.private_address?(params[:address]) }.to_json
+    end
+    run! if app_file == $PROGRAM_NAME
+  end
+end
